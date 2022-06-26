c0616ecfba84a1fa6ef199c8579c4b19caff8ba3
Richard Huang <flyerhzm@gmail.com>
2022-06-26 09:23:57 +0800

    abstract from synvert-core

---
+++ lib/parser_node_ext.rb
@@ -0,0 +1,239 @@
+# frozen_string_literal: true
+
+require_relative "parser_node_ext/version"
+
+require 'parser'
+
+module ParserNodeExt
+  class Error < StandardError; end
+
+  class MethodNotSupported < StandardError; end
+  # Your code goes here...
+
+  TYPE_CHILDREN = {
+    and: %i[left_value right_value],
+    arg: %i[name],
+    begin: %i[body],
+    block: %i[caller arguments body],
+    blockarg: %i[name],
+    const: %i[parent_const name],
+    class: %i[name parent_class body],
+    csend: %i[receiver message arguments],
+    cvasgn: %i[left_value right_value],
+    cvar: %i[name],
+    def: %i[name arguments body],
+    definded?: %i[arguments],
+    defs: %i[self name arguments body],
+    hash: %i[pairs],
+    ivasgn: %i[left_value right_value],
+    ivar: %i[name],
+    lvar: %i[name],
+    lvasgn: %i[left_value right_value],
+    masgn: %i[left_value right_value],
+    module: %i[name body],
+    or: %i[left_value right_value],
+    or_asgn: %i[left_value right_value],
+    pair: %i[key value],
+    restarg: %i[name],
+    send: %i[receiver message arguments],
+    super: %i[arguments],
+    zsuper: %i[]
+  }
+
+  def self.included(base)
+    base.class_eval do
+      # Initialize a Node.
+      #
+      # It extends {Parser::AST::Node} and set parent for its child nodes.
+      def initialize(type, children = [], properties = {})
+        @mutable_attributes = {}
+        super
+        # children could be nil for s(:array)
+        Array(children).each do |child_node|
+          if child_node.is_a?(Parser::AST::Node)
+            child_node.parent = self
+          end
+        end
+      end
+
+      # Get the parent node.
+      # @return [Parser::AST::Node] parent node.
+      def parent
+        @mutable_attributes[:parent]
+      end
+
+      # Set the parent node.
+      # @param node [Parser::AST::Node] parent node.
+      def parent=(node)
+        @mutable_attributes[:parent] = node
+      end
+
+      # Get the sibling nodes.
+      # @return [Array<Parser::AST::Node>] sibling nodes.
+      def siblings
+        index = parent.children.index(self)
+        parent.children[index + 1..]
+      end
+
+      # Dyamically defined method
+      # caller, key, left_value, message, name, pairs, parent_class, parent_const, receivr, rgith_value and value.
+      # based on const TYPE_CHILDREN.
+      %i[
+        caller
+        key
+        left_value
+        message
+        name
+        pairs
+        parent_class
+        parent_const
+        receiver
+        right_value
+        value
+      ].each do |method_name|
+        define_method(method_name) do
+          index = TYPE_CHILDREN[type]&.index(method_name)
+          return children[index] if index
+
+          raise Synvert::Core::MethodNotSupported, "#{method_name} is not handled for #{debug_info}"
+        end
+      end
+
+      # Return the left value of node.
+      # It supports :and, :cvagn, :lvasgn, :masgn, :or and :or_asgn nodes.
+      # @example
+      #   node # s(:or_asgn, s(:lvasgn, :a), s(:int, 1))
+      #   node.left_value # :a
+      # @return [Parser::AST::Node] left value of node.
+      # @raise [Synvert::Core::MethodNotSupported] if calls on other node.
+      def left_value
+        return children[0].children[0] if type == :or_asgn
+
+        index = TYPE_CHILDREN[type]&.index(:left_value)
+        return children[index] if index
+
+        raise Synvert::Core::MethodNotSupported, "#{left_value} is not handled for #{debug_info}"
+      end
+
+      # Get arguments of node.
+      # It supports :block, :csend, :def, :defined?, :defs and :send nodes.
+      # @example
+      #   node # s(:send, s(:const, nil, :FactoryGirl), :create, s(:sym, :post), s(:hash, s(:pair, s(:sym, :title), s(:str, "post"))))
+      #   node.arguments # [s(:sym, :post), s(:hash, s(:pair, s(:sym, :title), s(:str, "post")))]
+      # @return [Array<Parser::AST::Node>] arguments of node.
+      # @raise [Synvert::Core::MethodNotSupported] if calls on other node.
+      def arguments
+        case type
+        when :def, :block
+          children[1].children
+        when :defs
+          children[2].children
+        when :send, :csend
+          children[2..-1]
+        when :defined?
+          children
+        else
+          raise Synvert::Core::MethodNotSupported, "arguments is not handled for #{debug_info}"
+        end
+      end
+
+      # Get body of node.
+      # It supports :begin, :block, :class, :def, :defs and :module node.
+      # @example
+      #   node # s(:block, s(:send, s(:const, nil, :RSpec), :configure), s(:args, s(:arg, :config)), s(:send, nil, :include, s(:const, s(:const, nil, :EmailSpec), :Helpers)))
+      #   node.body # [s(:send, nil, :include, s(:const, s(:const, nil, :EmailSpec), :Helpers))]
+      # @return [Array<Parser::AST::Node>] body of node.
+      # @raise [Synvert::Core::MethodNotSupported] if calls on other node.
+      def body
+        case type
+        when :begin
+          children
+        when :def, :block, :class, :module
+          return [] if children[2].nil?
+
+          :begin == children[2].type ? children[2].body : children[2..-1]
+        when :defs
+          return [] if children[3].nil?
+
+          :begin == children[3].type ? children[3].body : children[3..-1]
+        else
+          raise Synvert::Core::MethodNotSupported, "body is not handled for #{debug_info}"
+        end
+      end
+
+      # Get condition of node.
+      # It supports :if node.
+      # @example
+      #   node # s(:if, s(:defined?, s(:const, nil, :Bundler)), nil, nil)
+      #   node.condition # s(:defined?, s(:const, nil, :Bundler))
+      # @return [Parser::AST::Node] condition of node.
+      # @raise [Synvert::Core::MethodNotSupported] if calls on other node.
+      def condition
+        if :if == type
+          children[0]
+        else
+          raise Synvert::Core::MethodNotSupported, "condition is not handled for #{debug_info}"
+        end
+      end
+
+      # Get keys of :hash node.
+      # @example
+      #   node # s(:hash, s(:pair, s(:sym, :foo), s(:sym, :bar)), s(:pair, s(:str, "foo"), s(:str, "bar")))
+      #   node.keys # [s(:sym, :foo), s(:str, "foo")]
+      # @return [Array<Parser::AST::Node>] keys of node.
+      # @raise [Synvert::Core::MethodNotSupported] if calls on other node.
+      def keys
+        if :hash == type
+          children.map { |child| child.children[0] }
+        else
+          raise Synvert::Core::MethodNotSupported, "keys is not handled for #{debug_info}"
+        end
+      end
+
+      # Get values of :hash node.
+      # @example
+      #   node # s(:hash, s(:pair, s(:sym, :foo), s(:sym, :bar)), s(:pair, s(:str, "foo"), s(:str, "bar")))
+      #   node.values # [s(:sym, :bar), s(:str, "bar")]
+      # @return [Array<Parser::AST::Node>] values of node.
+      # @raise [Synvert::Core::MethodNotSupported] if calls on other node.
+      def values
+        if :hash == type
+          children.map { |child| child.children[1] }
+        else
+          raise Synvert::Core::MethodNotSupported, "keys is not handled for #{debug_info}"
+        end
+      end
+    end
+  end
+end
+
+# Extend Parser::AST::Node.
+# {https://github.com/whitequark/parser/blob/master/lib/parser/ast/node.rb}
+#
+# Rules
+#
+# Synvert compares ast nodes with key / value pairs, each ast node has
+# multiple attributes, e.g. +receiver+, +message+ and +arguments+, it
+# matches only when all of key / value pairs match.
+#
+# +type: 'send', message: :include, arguments: ['FactoryGirl::Syntax::Methods']+
+#
+# Synvert does comparison based on the value type
+#
+# 1. if value is a symbol, then compares ast node value as symbol, e.g. +message: :include+
+# 2. if value is a string, then compares ast node original source code, e.g. +name: 'Synvert::Application'+
+# 3. if value is a regexp, then compares ast node original source code, e.g. +message: /find_all_by_/+
+# 4. if value is an array, then compares each ast node, e.g. +arguments: ['FactoryGirl::Syntax::Methods']+
+# 5. if value is nil, then check if ast node is nil, e.g. +arguments: [nil]+
+# 6. if value is true or false, then check if ast node is :true or :false, e.g. +arguments: [false]+
+# 7. if value is ast, then compare ast node directly, e.g. +to_ast: Parser::CurrentRuby.parse("self.class.serialized_attributes")+
+#
+# It can also compare nested key / value pairs, like
+#
+# +type: 'send', receiver: { type: 'send', receiver: { type: 'send', message: 'config' }, message: 'active_record' }, message: 'identity_map='+
+#
+# Source Code to Ast Node
+# {https://synvert-playground.xinminlabs.com/ruby}
+class Parser::AST::Node
+  include ParserNodeExt
+end
---
+++ spec/parser_node_ext_spec.rb
@@ -0,0 +1,346 @@
+# frozen_string_literal: true
+
+require 'parser/current'
+
+RSpec.describe ParserNodeExt do
+  def parse(code)
+    Parser::CurrentRuby.parse code
+  end
+
+  describe '#parent' do
+    it 'gets parent node' do
+      node = parse('FactoryBot.create(:user)')
+      child_node = node.children.first
+      expect(child_node.parent).to eq node
+    end
+  end
+
+  describe '#siblings' do
+    it 'gets sibling nodes' do
+      node = parse(<<~EOS)
+        def foobar; end
+        def foo; end
+        def bar; end
+      EOS
+      expect(node.children.first.siblings).to eq [node.children[1], node.children[-1]]
+    end
+  end
+
+  describe '#name' do
+    it 'gets for class node' do
+      node = parse('class Synvert; end')
+      expect(node.name).to eq parse('Synvert')
+
+      node = parse('class Synvert::Rewriter::Instance; end')
+      expect(node.name).to eq parse('Synvert::Rewriter::Instance')
+    end
+
+    it 'gets for module node' do
+      node = parse('module Synvert; end')
+      expect(node.name).to eq parse('Synvert')
+    end
+
+    it 'gets for def node' do
+      node = parse('def current_node; end')
+      expect(node.name).to eq :current_node
+    end
+
+    it 'gets for defs node' do
+      node = parse('def self.current_node; end')
+      expect(node.name).to eq :current_node
+    end
+
+    it 'gets for arg node' do
+      node = parse('def test(foo); end')
+      expect(node.arguments.first.name).to eq :foo
+    end
+
+    it 'gets for blockarg node' do
+      node = parse('def test(&block); end')
+      expect(node.arguments.first.name).to eq :block
+    end
+
+    it 'gets for const node' do
+      node = parse('Synvert')
+      expect(node.name).to eq :Synvert
+    end
+
+    it 'gets for lvar node' do
+      node = parse("foo = 'bar'\nfoo").children[1]
+      expect(node.name).to eq :foo
+    end
+
+    it 'gets for ivar node' do
+      node = parse("@foo = 'bar'\n@foo").children[1]
+      expect(node.name).to eq :@foo
+    end
+
+    it 'gets for cvar node' do
+      node = parse("@@foo = 'bar'\n@@foo").children[1]
+      expect(node.name).to eq :@@foo
+    end
+
+    it 'gets for restarg node' do
+      node = parse('object.each { |*entry| }')
+      expect(node.arguments.first.name).to eq :entry
+    end
+  end
+
+  describe '#parent_class' do
+    it 'gets for class node' do
+      node = parse('class Post < ActiveRecord::Base; end')
+      expect(node.parent_class).to eq parse('ActiveRecord::Base')
+    end
+  end
+
+  describe '#receiver' do
+    it 'gets for send node' do
+      node = parse('FactoryGirl.create :post')
+      expect(node.receiver).to eq parse('FactoryGirl')
+    end
+
+    it 'gets for csend node' do
+      node = parse('user&.update(name: name)')
+      expect(node.receiver).to eq parse('user')
+    end
+  end
+
+  describe '#message' do
+    it 'gets for send node' do
+      node = parse('FactoryGirl.create :post')
+      expect(node.message).to eq :create
+    end
+
+    it 'gets for csend node' do
+      node = parse('user&.update(name: name)')
+      expect(node.message).to eq :update
+    end
+  end
+
+  describe '#parent_const' do
+    it 'gets for const node' do
+      node = parse('Synvert::Node')
+      expect(node.parent_const).to eq parse('Synvert')
+    end
+
+    it 'gets for const node at the root' do
+      node = parse('::Node')
+      expect(node.parent_const.type).to eq :cbase
+    end
+
+    it 'gets for const node with no parent' do
+      node = parse('Node')
+      expect(node.parent_const).to eq nil
+    end
+  end
+
+  describe '#arguments' do
+    it 'gets for def node' do
+      node = parse('def test(foo, bar); foo + bar; end')
+      expect(node.arguments.map(&:type)).to eq [:arg, :arg]
+    end
+
+    it 'gets for defs node' do
+      node = parse('def self.test(foo, bar); foo + bar; end')
+      expect(node.arguments.map(&:type)).to eq [:arg, :arg]
+    end
+
+    it 'gets for block node' do
+      node = parse('RSpec.configure do |config|; end')
+      expect(node.arguments.map(&:type)).to eq [:arg]
+    end
+
+    it 'gets for send node' do
+      node = parse("FactoryGirl.create :post, title: 'post'")
+      expect(node.arguments).to eq parse("[:post, title: 'post']").children
+    end
+
+    it 'gets for csend node' do
+      node = parse('user&.update(name: name)')
+      expect(node.arguments).to eq parse('[name: name]').children
+    end
+
+    it 'gets for defined? node' do
+      node = parse('defined?(Bundler)')
+      expect(node.arguments).to eq [parse('Bundler')]
+    end
+  end
+
+  describe '#caller' do
+    it 'gets for block node' do
+      node = parse('RSpec.configure do |config|; end')
+      expect(node.caller).to eq parse('RSpec.configure')
+    end
+  end
+
+  describe '#body' do
+    it 'gets one line for block node' do
+      node = parse('RSpec.configure do |config|; include EmailSpec::Helpers; end')
+      expect(node.body).to eq [parse('include EmailSpec::Helpers')]
+    end
+
+    it 'gets multiple lines for block node' do
+      node = parse('RSpec.configure do |config|; include EmailSpec::Helpers; include EmailSpec::Matchers; end')
+      expect(node.body).to eq [parse('include EmailSpec::Helpers'), parse('include EmailSpec::Matchers')]
+    end
+
+    it 'gets empty for class node' do
+      node = parse('class User; end')
+      expect(node.body).to be_empty
+    end
+
+    it 'gets empty for module node' do
+      node = parse('module Admin; end')
+      expect(node.body).to be_empty
+    end
+
+    it 'gets one line for class node' do
+      node = parse('class User; attr_accessor :email; end')
+      expect(node.body).to eq [parse('attr_accessor :email')]
+    end
+
+    it 'gets one line for class node' do
+      node = parse('class User; attr_accessor :email; attr_accessor :username; end')
+      expect(node.body).to eq [parse('attr_accessor :email'), parse('attr_accessor :username')]
+    end
+
+    it 'gets for begin node' do
+      node = parse('foo; bar')
+      expect(node.body).to eq [parse('foo'), parse('bar')]
+    end
+
+    it 'gets for def node' do
+      node = parse('def test; foo; bar; end')
+      expect(node.body).to eq [parse('foo'), parse('bar')]
+    end
+
+    it 'gets for def node with empty body' do
+      node = parse('def test; end')
+      expect(node.body).to eq []
+    end
+
+    it 'gets for defs node' do
+      node = parse('def self.test; foo; bar; end')
+      expect(node.body).to eq [parse('foo'), parse('bar')]
+    end
+
+    it 'gets for def node with empty body' do
+      node = parse('def self.test; end')
+      expect(node.body).to eq []
+    end
+  end
+
+  describe '#keys' do
+    it 'gets for hash node' do
+      node = parse("{:foo => :bar, 'foo' => 'bar'}")
+      expect(node.keys).to eq [parse(':foo'), parse("'foo'")]
+    end
+  end
+
+  describe '#values' do
+    it 'gets for hash node' do
+      node = parse("{:foo => :bar, 'foo' => 'bar'}")
+      expect(node.values).to eq [parse(':bar'), parse("'bar'")]
+    end
+  end
+
+  describe '#key' do
+    it 'gets for pair node' do
+      node = parse("{:foo => 'bar'}").children[0]
+      expect(node.key).to eq parse(':foo')
+    end
+  end
+
+  describe '#value' do
+    it 'gets for hash node' do
+      node = parse("{:foo => 'bar'}").children[0]
+      expect(node.value).to eq parse("'bar'")
+    end
+  end
+
+  describe '#condition' do
+    it 'gets for if node' do
+      node = parse('if defined?(Bundler); end')
+      expect(node.condition).to eq parse('defined?(Bundler)')
+    end
+  end
+
+  describe '#left_value' do
+    it 'gets for masgn' do
+      node = parse('a, b = 1, 2')
+      expect(node.left_value.type).to eq :mlhs
+    end
+
+    it 'gets for lvasgn' do
+      node = parse('a = 1')
+      expect(node.left_value).to eq :a
+    end
+
+    it 'gets for ivasgn' do
+      node = parse('@a = 1')
+      expect(node.left_value).to eq :@a
+    end
+
+    it 'gets for cvasgn' do
+      node = parse('@@a = 1')
+      expect(node.left_value).to eq :@@a
+    end
+
+    it 'gets for or_asgn' do
+      node = parse('a ||= 1')
+      expect(node.left_value).to eq :a
+    end
+
+    it 'gets for and' do
+      node = parse('foo && bar')
+      expect(node.left_value).to eq parse('foo')
+    end
+
+    it 'gets for or' do
+      node = parse('foo || bar')
+      expect(node.left_value).to eq parse('foo')
+    end
+  end
+
+  describe '#right_value' do
+    it 'gets for masgn' do
+      node = parse('a, b = 1, 2')
+      expect(node.right_value).to eq parse('[1, 2]')
+    end
+
+    it 'gets for masgn' do
+      node = parse('a, b = params')
+      expect(node.right_value).to eq parse('params')
+    end
+
+    it 'gets for lvasgn' do
+      node = parse('a = 1')
+      expect(node.right_value).to eq parse('1')
+    end
+
+    it 'gets for ivasgn' do
+      node = parse('@a = 1')
+      expect(node.right_value).to eq parse('1')
+    end
+
+    it 'gets for cvasgn' do
+      node = parse('@@a = 1')
+      expect(node.right_value).to eq parse('1')
+    end
+
+    it 'gets for or_asgn' do
+      node = parse('a ||= 1')
+      expect(node.right_value).to eq parse('1')
+    end
+
+    it 'gets for and' do
+      node = parse('foo && bar')
+      expect(node.right_value).to eq parse('bar')
+    end
+
+    it 'gets for or' do
+      node = parse('foo || bar')
+      expect(node.right_value).to eq parse('bar')
+    end
+  end
+end
